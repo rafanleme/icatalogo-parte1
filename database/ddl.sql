4970f7685056608574742ad67e9f082b86e0fba9
unknown <rafanleme@gmail.com>
2021-04-29 10:44:48 -0300

    Correção no DDL do banco

--- database/ddl.sql
+++ database/ddl.sql
@@ -5,11 +5,13 @@ use icatalogo;
 create table tbl_produto(
     id int primary key auto_increment,
     descricao varchar(255) not null,
-    peso decimal not null,
+    peso decimal(10,2) not null,
     quantidade int not null,
     cor varchar(100) not null,
     tamanho varchar(100),
-    valor decimal not null,
+    valor decimal(10,2) not null,
     desconto int,
     imagem varchar(500)
 );
+
+drop table tbl_produto;
